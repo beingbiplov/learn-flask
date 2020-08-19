8e1dbbbbb4a59d9623d3d30eb8b3afe9f76060b3
beingbiplov <beingbiplov@gmail.com>
2020-08-19 15:33:32 +0545

    dynamic route

--- hello.py
+++ hello.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return '<h1>Hello World!</h1>'
+
+# Dynamic route to make app respond with personalized message.
+@app.route('/user/<name>')
+def user(name):
+	return f'<h3>Hello { name } !!'
