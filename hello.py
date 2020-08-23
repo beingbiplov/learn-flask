18427737d7cc4c7601fba727bb8425aa50017ccb
beingbiplov <beingbiplov@gmail.com>
2020-08-23 14:26:54 +0545

    use database in view function

--- hello.py
+++ hello.py
@@ -24,18 +24,26 @@ class NameForm(FlaskForm):
 	name = StringField("Enter your name", validators=[DataRequired()])
 	submut = SubmitField('Submit')
 
-# Handling form
+# use of databse in view
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Your name is now changed!')
-		session['name'] = form.name.data 
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			db.session.commit()
+			session['known'] = False
+		else:
+			session['known'] = True
+		session['name'] = form.name.data
+		form.name.data= ''
 		return redirect(url_for('index'))
-	return render_template('index.html', form = form, name=session.get('name'))
+	return render_template('index.html', 
+							form = form, 
+							name=session.get('name'),
+							known = session.get('known', False))
 	
 # # Dynamic route to make app respond with personalized message.
 @app.route('/user/<name>')
@@ -56,7 +64,7 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
-	users = db.relationship('User', backref='role')
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
