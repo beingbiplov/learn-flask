3166920631b8f8c7639f2c17291446425390903b
beingbiplov <beingbiplov@gmail.com>
2020-08-23 11:09:29 +0545

    add web forms

--- hello.py
+++ hello.py
@@ -2,18 +2,29 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '12abc'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-# rendering template
+class NameForm(FlaskForm):
+	name = StringField("Enter your name", validators=[DataRequired()])
+	submut = SubmitField('Submit')
 
-@app.route('/')
+# Handling form
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html',
-							current_time = datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data 
+		form.name.data = ''
+	return render_template('index.html', form = form, name=name)
 	
 # # Dynamic route to make app respond with personalized message.
 @app.route('/user/<name>')
@@ -28,3 +39,4 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
